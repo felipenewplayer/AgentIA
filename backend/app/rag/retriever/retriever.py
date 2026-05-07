1064358489eaf3fa83a88926e4fc09369fb447e4
felipenewplayer <felipeapereira2014@gmail.com>
2026-05-06 22:17:04 -0300

    mudando estrutura

---
+++ backend/app/rag/retriever/retriever.py
@@ -0,0 +1,9 @@
+from app.rag.vectorstore.chroma_store import get_vector_store
+
+def get_retriever():
+
+    vector_store = get_vector_store()
+
+    return vector_store.as_retriever(
+        search_kwargs={"k": 5}
+    )
